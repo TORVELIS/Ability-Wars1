1d7aa849bd072dd94cd2516741d58e0c5619f3cb
TORVELIS <89320445+TORVELIS@users.noreply.github.com>
2022-11-26 00:53:01 +0200

    Create Ability Wars2.lua

--- Ability Wars2.lua
+++ Ability Wars2.lua
@@ -13,6 +13,30 @@ if CoreGui:FindFirstChild("PPHUD") then
     DestroyGui()
 end
 
+local function PlatformOnSpawn(Selected)
+   if Selected == "Spawn" then
+       local Cl = game:GetService("Workspace").Main["Map Base"].Mainplate:Clone()
+   
+       Cl.Parent = game.Workspace
+       Cl.Name = "SpawnPlatformlol"
+       
+       Cl.CFrame = Cl.CFrame * CFrame.new(Vector3.new(0,-50,0))
+       Cl.Size = Vector3.new(1060,1,1060)
+       Cl.Transparency = 0.5
+
+   elseif Selected == "Spleef" then
+      local Cl = game:GetService("Workspace").Main["Map Base"].Mainplate:Clone()
+      Cl.Parent = game.Workspace
+      Cl.Name = "SpleefPlatformFuckerEzKidLoL"
+      
+      Cl.CFrame = Cl.CFrame * CFrame.new(Vector3.new(-19117.9746, -26.96139812, -57.5339622, -0.981866181, -7.51963753e-08, -0.189575449, -7.40857189e-08, 1, -1.29453523e-08, 0.189575449, 1.33422928e-09, -0.981866181))
+      Cl.Size = Vector3.new(1060,1,1060)
+      Cl.Transparency = 0.5
+
+   end
+end
+
+
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/TORVELIS/PPHub/main/Lib.lua'))()
 local Flags = Library.Flags
 
@@ -67,11 +91,25 @@ local Section3 = Tab2:Section({
     Side = "Right"
 })
 
-Section3:Button({
-    Text = "Add platforms",
-    Callback = function()
-        print("")
-    end
+Section3:Dropdown({
+Text = "Platform",
+List = {"Arena Platform", "Spawn Platform", "Spleef Platform"},
+Callback = function(Platform)
+   getgenv().PlatformAdd = Platform
+end
 })
 
+Section3:Button({
+Text = "Add Platform",
+Callback = function()
+    if getgenv().PlatformAdd == "Arena Platform" then
+    print("Arena added")
+    elseif getgenv().PlatformAdd == "Spawn Platform" then
+      PlatformOnSpawn("Spawn")
+    elseif getgenv().PlatformAdd == "Spleef Platform" then
+      PlatformOnSpawn("Spleef")
+    
+    end
+end
+})
 Tab:Select()
